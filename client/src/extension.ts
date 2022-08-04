71ef74b8e7a3e139349a1b8d39918d81ae3bc7ee
Ryo Nakamura <r7kamura@gmail.com>
2022-08-05 07:03:59 +0900

    Change language server client id and name

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -46,12 +46,7 @@ export function activate(context: ExtensionContext) {
   };
 
   // Create the language client and start the client.
-  client = new LanguageClient(
-    "languageServerExample",
-    "Language Server Example",
-    serverOptions,
-    clientOptions
-  );
+  client = new LanguageClient("ruby", "Ruby", serverOptions, clientOptions);
 
   // Start the client. This will also launch the server
   client.start();

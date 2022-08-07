80c781f4e7f9900aeb2e29098d82b3e5f7f89c3c
Ryo Nakamura <r7kamura@gmail.com>
2022-08-07 18:08:38 +0900

    Add eslint-plugin-unused-imports (#6)

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "unused-imports"],
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   rules: {
     semi: [2, "always"],
@@ -11,5 +11,6 @@ module.exports = {
     "@typescript-eslint/no-explicit-any": 0,
     "@typescript-eslint/explicit-module-boundary-types": 0,
     "@typescript-eslint/no-non-null-assertion": 0,
+    "unused-imports/no-unused-imports": "error",
   },
 };
